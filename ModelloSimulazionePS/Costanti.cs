2297ffbfb6c169e21629a6d5da4bb10e2159090f
cesco8388 <cesco8388@hotmail.it>
2019-11-23 19:22:32 +0100

    Creazione variabili di base

--- ModelloSimulazionePS/Costanti.cs
+++ ModelloSimulazionePS/Costanti.cs
@@ -7,5 +7,21 @@ namespace ModelloSimulazionePS
     static public class  Costanti
     {
         public const int TempoGestioneB = 10;
+        public const int TempoGestioneV = 20;
+        public const int TempoGestioneG = 30;
+        public const int TempoGestioneR = 60;
+        public const int MaxPzInCarico = 10;
+        public const int NumeroMediciBianco = 1; //2
+        public const int NumeroMeidiciRosso = 1; //0
+        public const int TempoConsegne = 10;
+        public const string OrarioCambioMattino = "08:00:00";
+        public const string OrarioCambioSera = "20:00:00";
+        public const int TempoPermanenzaPSB = 30;
+        public const int TempoPermanenzaPSV = 120;
+        public const int TempoPermanenzaPSG = 120;
+        public const int TempoPermanenzaPSR = 90;
+        public const int TempoUscita108 = 90;
+        public const int NumeroUscite108 = 3; //4
+        public const bool DivisioneCodici = false; //true
     }
 }
--- ModelloSimulazionePS/Program.cs
+++ ModelloSimulazionePS/Program.cs
@@ -1,27 +1,68 @@
 ﻿using System;
 
+//Istruzione per utilizzare i tipi generici
+using System.Collections.Generic;
+
 namespace ModelloSimulazionePS
 {
+    //Classe del flusso principale del programma
     class Program
     {
         static void Main(string[] args)
         {
+            InizializzazioneVariabili();
+            
             int tre = Costanti.TempoGestioneB;
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
+
+        private static void InizializzazioneVariabili()
+        {
+            List<Paziente> ListaPzInArrivo = new List<Paziente>();
+            List<Paziente> ListaPzInAttesa = new List<Paziente>();
+            DateTime orarioAttuale = DateTime.Parse("01/01/2018 00:00");
+            List<ElementoLista> listaStampaPzAttesa = new List<ElementoLista>();
+            List<ElementoLista> listaStampaPzGestione = new List<ElementoLista>();
+            List<Medico> ListaMedici = new List<Medico>();
+        }
     }
 
+    //Classe che modella un medico
     class Medico
     {
-        char tipologia;
-        bool impegnato;
-
+        char tipologia; //'B' 'R'
+        bool occupato = false;
+        List<Paziente> PzInCarico = new List<Paziente>();
+        DateTime orarioFineOccupazione = new DateTime();
     }
 
+    //Classe che modella un paziente
     class Paziente
     {
-        char codice;
+        char codice; //'B' 'V' 'G' 'R'
+        DateTime dataIngresso = new DateTime();
+        DateTime dataGestione = new DateTime();
+        DateTime dataUscita = new DateTime();
         
+        bool dimissibile()
+        {
+            return false;
+        }
+    }
+
+    //Classe che modella gli elementi nelle liste di stampa
+    class ElementoLista
+    {
+        DateTime orario = new DateTime();
+        int numeroB = 0;
+        int numeroV = 0;
+        int numeroG = 0;
+        int numeroR = 0;
+
+        int numeroTotali()
+        {
+            return 0;
+        }
     }
 }
