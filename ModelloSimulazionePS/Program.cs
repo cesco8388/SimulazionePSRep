872b29a3b39dd410eba53ee0f39637a994997ea4
cesco8388 <cesco8388@hotmail.it>
2019-11-23 18:04:53 +0100

    creazione dei file iniziali

---
+++ ModelloSimulazionePS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelloSimulazionePS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
