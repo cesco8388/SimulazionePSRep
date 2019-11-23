272907d54ee17e4b801aa9814572b1b8f288e053
cesco8388 <cesco8388@hotmail.it>
2019-11-23 18:18:56 +0100

    Aggiunta costanti

---
+++ ModelloSimulazionePS/Costanti.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelloSimulazionePS
+{
+    static public class  Costanti
+    {
+        public const int TempoGestioneB = 10;
+    }
+}
--- ModelloSimulazionePS/Program.cs
+++ ModelloSimulazionePS/Program.cs
@@ -6,7 +6,22 @@ namespace ModelloSimulazionePS
     {
         static void Main(string[] args)
         {
+            int tre = Costanti.TempoGestioneB;
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
+    }
+
+    class Medico
+    {
+        char tipologia;
+        bool impegnato;
+
+    }
+
+    class Paziente
+    {
+        char codice;
+        
     }
 }
