01f16ceb13d201fe5e6d540b16a32c356ab64f67
cesco8388 <cesco8388@hotmail.it>
2019-11-27 00:00:50 +0100

    aggiunta prova di gestione dei file txt

--- ModelloSimulazionePS/Program.cs
+++ ModelloSimulazionePS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 //Istruzione per utilizzare i tipi generici
 using System.Collections.Generic;
@@ -10,17 +11,36 @@ namespace ModelloSimulazionePS
     {
         static void Main(string[] args)
         {
+            //Inizializzazione delle variabili della simulazione
             InizializzazioneVariabili();
-            
-            int tre = Costanti.TempoGestioneB;
-            Console.WriteLine("Hello World!");
+
+            //StreamReader streamReader = new StreamReader("risultatoTesto.txt");
+
+            List<string> stringe = new List<string>();
+            stringe.Add("uno");
+            stringe.Add("duer");
+
+            //string stringa = "";
+            //stringa = streamReader.ReadLine();
+            //string[] lines = { "First line", "Second line", "Third line" };
+            //System.IO.File.WriteAllLines(@"C:\Users\cesco\Source\repos\SimulazionePSRep\ModelloSimulazionePS\risultatoTesto.txt", lines);
+
+            //Scrive il contenuto della lista (ogni elemento in una riga nuova) nel file messo nel percorso (si deve specificare tutto il percorso
+            //se il file non esiste viene creato. tutto quello è scritto viene sovrascitto
+            System.IO.File.WriteAllLines(@"C:\Users\cesco\Source\repos\SimulazionePSRep\ModelloSimulazionePS\risultatoTesgto.txt", stringe);
+
+
+
+            //int tre = Costanti.TempoGestioneB;
+            //Console.WriteLine(stringa);
             Console.ReadLine();
         }
 
+        //Metodo per l'inizializzazione delle variabili usate nella simulazione
         private static void InizializzazioneVariabili()
         {
-            List<Paziente> ListaPzInArrivo = new List<Paziente>();
-            List<Paziente> ListaPzInAttesa = new List<Paziente>();
+            Dictionary<int, Paziente> ListaPzInArrivo = new Dictionary<int, Paziente>();
+            Dictionary<int, Paziente> ListaPzInAttesa = new Dictionary<int, Paziente>();
             DateTime orarioAttuale = DateTime.Parse("01/01/2018 00:00");
             List<ElementoLista> listaStampaPzAttesa = new List<ElementoLista>();
             List<ElementoLista> listaStampaPzGestione = new List<ElementoLista>();
@@ -33,7 +53,7 @@ namespace ModelloSimulazionePS
     {
         char tipologia; //'B' 'R'
         bool occupato = false;
-        List<Paziente> PzInCarico = new List<Paziente>();
+        Dictionary<int, Paziente> PzInCarico = new Dictionary<int, Paziente>();
         DateTime orarioFineOccupazione = new DateTime();
     }
 
